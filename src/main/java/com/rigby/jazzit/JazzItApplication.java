0cce9ff9be0e42bbdbdc460587c7e34eca06b010
asakopako <asakopako86@gmail.com>
2020-04-18 03:12:10 +0200

    Initial import

---
+++ src/main/java/com/rigby/jazzit/JazzItApplication.java
@@ -0,0 +1,13 @@
+package com.rigby.jazzit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazzItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JazzItApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rigby/jazzit/JazzItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rigby.jazzit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazzItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
